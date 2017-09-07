f9464445986f28b70a3a127d6bad08954f5cb4ac
Pete Kneller <mrkneller@hotmail.com>
2017-09-07 08:49:06 +0100

    Fix mistake in the formula for law of cosines

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import matplotlib.pyplot as plot
 import math
 import numpy as np
 
-swingarm_length = 500
+swingarm_length = 430
 swingarm_angle_from_horz_full_shock_extension = -30
 shock_spring_rate = 1000 / (2.2 * 25.4) # 1000 lbs/in -> kg/mm
 
 # angle between swingarm and the line between swingarm pivot and lower susp. mount. This needs to be factored into the angle between swingarm and upper pivot
 swingarm_pivot_vert_to_lower_mount = 50.0
-swingarm_pivot_horz_to_lower_mount = 200.0
+swingarm_pivot_horz_to_lower_mount = 100.0
 swingarm_lower_mount_included_angle = math.atan(swingarm_pivot_vert_to_lower_mount / swingarm_pivot_horz_to_lower_mount) * 180 / math.pi
 swingarm_pivot_to_lower_mount = math.sqrt(swingarm_pivot_vert_to_lower_mount ** 2 + swingarm_pivot_horz_to_lower_mount ** 2)
 
 # calculate angle between suspension mounts at resting position
-swingarm_pivot_to_upper_mount = 200.0
+swingarm_pivot_to_upper_mount = 250.0
 upper_mount_to_lower_mount_resting = 270.0
 
 def angleBetweenMountsResting():
     a = upper_mount_to_lower_mount_resting
     b = swingarm_pivot_to_upper_mount
     c = swingarm_pivot_to_lower_mount
-    alpha = math.acos((b**2) + (c**2) - (a**2) / (2*b*c))
+    alpha = math.acos(((b**2) + (c**2) - (a**2)) / (2*b*c))
     return alpha
 
 swingarm_upper_mount_angle_from_horz = swingarm_angle_from_horz_full_shock_extension + swingarm_lower_mount_included_angle + angleBetweenMountsResting()
